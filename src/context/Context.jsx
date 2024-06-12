7c270332289ca26f23590ba724365a46fc71d155
Rania Benamara <raniabenamara1101@gmail.com>
2024-06-12 11:14:10 -0400

    fetch data and show result

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,23 +2,54 @@ import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 
-const Context = createContext ();
+export const Context = createContext ();
 
 const ContextProvider = (props) => {
 
   const [input,setInput] = useState("");
-  const [recetPrompt,setRecentPrompt] = useState("");
+  const [recentPrompt,setRecentPrompt] = useState("");
   const[prevPrompt,setPrevPrompt]= useState([]);
   const [showResult,setShowResult] = useState(false);
   const[loading,setLoading] = useState(false);
   const[resultData,setResultData] = useState("");
+   
+  const delayPara = (index,nextWord) => {
 
-  const onSent = async (prompt) => {
-    await run(prompt)
   }
 
- 
+  const onSent = async (prompt) => {
+    setResultData("")
+    setLoading(true)
+    setShowResult(true)
+    setRecentPrompt(input)
+    const response = await run(input)
+    let responseArray = response.split("**");
+    let newResponse;
+    for(let i=0; i< responseArray.length; i++){
+      if(i === 0 || i%2 !==1){
+          newResponse += responseArray[i];
+      }else{
+        newResponse += "<b>" +responseArray[i] +"</b>";
+      }
+    }
+
+    setResultData(newResponse)
+    setLoading(false)
+    setInput("")
+  }
+
+  
   const contextValue = {
+      prevPrompt,
+      setPrevPrompt,
+      onSent,
+      setRecentPrompt,
+      recentPrompt,
+      showResult,
+      loading,
+      resultData,
+      input,
+      setInput
 
   }
 
